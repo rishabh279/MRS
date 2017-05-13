e8a8066ae595fe34a576045dac6baa071837f361
rajatsaxena035 <rajat.saxena035@gmail.com>
2017-05-14 01:21:21 +0530

    Added gui file

---
+++ gui/home.py
@@ -0,0 +1,33 @@
+from Tkinter import *
+
+master = Tk()
+master.attributes('-zoomed', True)
+
+frame = Frame(master)
+frame.pack()
+
+listbox = Listbox(frame)
+listbox.pack(side="left", fill="y")
+
+scrollbar = Scrollbar(frame, orient="vertical")
+scrollbar.config(command=listbox.yview)
+scrollbar.pack(side="right", fill="y")
+
+listbox.config(yscrollcommand=scrollbar.set)
+
+for item in ["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]:
+    listbox.insert(END, item)
+
+var = StringVar()
+label = Message( master, textvariable=var )
+
+def recommend():
+	
+	sel = listbox.curselection()	
+	var.set(str(sel)+ "is selected")
+		
+b = Button(master, text="Recommend", command=recommend)
+b.pack()
+label.pack()
+
+mainloop()
